7883272f6d54bcde92ca41fb9c81bbb5db0b281e
cookie1981 <david.cook@comparethemarket.com>
2015-04-21 10:15:46 +0100

    Dave - Added examples of ISP being broken

---
+++ ISP/IVehcile.cs
@@ -0,0 +1,25 @@
+﻿namespace BreakingInterfaceSegregationPrinciple
+{
+    public interface IVehcile
+    {
+        void Accelerate();
+        void Break();
+        void ChangeGear(int gear);
+        void TurnOnRadio();
+        void TurnOffRadio();
+        void OpenSunRoof(int percentOpen);
+        void CloseSunRoof();
+    }
+
+    #region What's wrong with the above?
+// At first glance it seems fine… nothing bad going on here.
+// Or is there?
+//
+// Lets think about when we start building concrete implementations of this interface.
+// An LuxuryCar class would be ok, as it can meet the contract set by the interface… 
+//     However, there is going to be a lot of code in this class (potentially breaking SRP)
+// And what about a MotorCycle, or a Bus, or a budget car class? 
+// None of these need to implement all the methods required by the contract, so 
+// we are forced to Throw Exceptions when these methods are called - and therefore write code to handle those exceptions!
+    #endregion
+}
---
+++ ISP/Vehicles.cs
@@ -0,0 +1,111 @@
+namespace BreakingInterfaceSegregationPrinciple
+{
+    public class LuxuryCar : IVehcile
+    {
+        private bool _radioOn;
+
+        public void Accelerate(){ 
+            // do something to make car go faster
+        }
+
+        public void Break(){
+            //do something to make car slow down
+        }
+
+        public void ChangeGear(int gear){
+            // change to selected gear
+        }
+
+        public void TurnOnRadio(){
+            _radioOn = true;
+        }
+        public void TurnOffRadio(){
+            _radioOn = false;
+        }
+
+        public void OpenSunRoof(int percentOpen){
+            //Open sunroof to requested amount
+        }
+
+        public void CloseSunRoof(){
+            //Make sunroof 100% shut (0% open)
+        }
+    }
+
+    public class MotorCycle : IVehcile
+    {
+        public void Accelerate()
+        {
+            // do something to make Bike go faster 
+        }
+
+        public void Break()
+        {
+            // do something to make Bike slow down 
+        }
+
+        public void ChangeGear(int gear)
+        {
+            //change to selected gear...remember that bikes have a sequential gear box!
+        }
+
+        public void TurnOnRadio()
+        {
+            throw new System.NotImplementedException("I don't have a radio - I'm a motorcycle!");
+        }
+
+        public void TurnOffRadio()
+        {
+            throw new System.NotImplementedException("I don't have a radio - I'm a motorcycle!");
+        }
+
+        public void OpenSunRoof(int percentOpen)
+        {
+            throw new System.NotImplementedException("I don't have a Sunroof - I'm a motorcycle!");
+        }
+
+        public void CloseSunRoof()
+        {
+            throw new System.NotImplementedException("I don't have a Sunroof - I'm a motorcycle!");
+        }
+    }
+
+    public class BudgetCar : IVehcile
+    {
+        private bool _radioOn;
+
+        public void Accelerate()
+        {
+            // do something to make car go faster
+        }
+
+        public void Break()
+        {
+            //do something to make car slow down
+        }
+
+        public void ChangeGear(int gear)
+        {
+            // change to selected gear
+        }
+
+        public void TurnOnRadio()
+        {
+            _radioOn = true;
+        }
+        public void TurnOffRadio()
+        {
+            _radioOn = false;
+        }
+
+        public void OpenSunRoof(int percentOpen)
+        {
+            throw new System.NotImplementedException("I don't have a Sunroof - It was an optional extra and you didn't shell out for it!");
+        }
+
+        public void CloseSunRoof()
+        {
+            throw new System.NotImplementedException("I don't have a Sunroof - It was an optional extra and you didn't shell out for it!");
+        }
+    }
+}
