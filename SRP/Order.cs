b1401810c9fa79f4460a91943e5f215eb87758e2
FlaviusAetius <FlaviusAetius@users.noreply.github.com>
2015-06-08 10:54:26 +0100

    Add order class to SRP bad code example

---
+++ SRP/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BreakingSingleResponsibility
+{
+    public class Order
+    {
+        public void PlaceOrder()
+        {
+            try
+            {
+                // Code to add order to database would be here...
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"C:\Error.txt", ex.ToString());
+            }
+        }
+    }
+}
